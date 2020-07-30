36ef8af139e43ec258b99f0a2ff48c8445095d0e
krgai <k.r.gaik@gmail.com>
2020-07-30 07:50:03 +0200

    packages

---
+++ src/main/java/karolina/projects/online_store_project/OnlineStoreProjectApplication.java
@@ -0,0 +1,13 @@
+package karolina.projects.online_store_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineStoreProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/karolina/projects/online_store_project/OnlineStoreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package karolina.projects.online_store_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
